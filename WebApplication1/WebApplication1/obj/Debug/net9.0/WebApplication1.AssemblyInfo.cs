d0a3ac33d259e80e1251354ed4966505fdb5f84e
Caydn Baldwin <cbaldwin@eidebailly.com>
2025-05-12 10:14:34 -0600

    ConsoleApp

--- WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9da3501ee546dbaff0b669d75f50731df0611dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bbbd69cd01a2d5a7083193002aa84ad70d88866")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

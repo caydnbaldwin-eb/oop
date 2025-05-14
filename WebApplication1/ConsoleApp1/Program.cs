18aa7124fac365cdaa7ee5e857181fb209916ed1
Caydn Baldwin <cbaldwin@eidebailly.com>
2025-05-14 11:43:17 -0600

    Final code

--- WebApplication1/ConsoleApp1/Program.cs
+++ WebApplication1/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq.Expressions;
 
 namespace ConsoleApp1
 {
@@ -13,23 +14,45 @@ namespace ConsoleApp1
             );
         }
 
+        static void Report(Child[] children, SchoolSystem school)
+        {
+            foreach (Child child in children)
+                Report(child, school);
+        }
+
         static void Demonstrate(Calendar cal)
         {
-            SchoolSystem school = new SchoolSystem(cal.Create(3, 1), 5, cal.Create(8, 15));
+            SchoolSystem school = new(cal.Create(3, 1), 5, cal.Create(8, 15));
 
-            Child jill = new Child("Jill", new Date(cal, 1892, cal.Create(2, 29)));
-            Child jake = new Child("Jake", new Date(cal, 1891, cal.Create(8, 27)));
+            Child[] children = new Child[]
+            {
+                new Child("Jill", cal.Create(2008, 2, 29)),
+                new Child("Jake", cal.Create(2007, 8, 27)),
+                new Child("Jimmy", cal.Create(2008, 1, 31)),
+                new Child("Jane", cal.Create(2009, 4, 22)),
+                new Child("John", cal.Create(2007, 11, 14))
+            };
 
-            Report(jill, school);
-            Report(jake, school);
+            Report(children, school);
             Console.WriteLine();
         }
 
+        static void AddCalendars(List<Calendar> calendars, JulianCalendar calendar) => 
+            calendars.Add(calendar);
+
         static void Main(string[] args)
         {
-            Demonstrate(new GregorianCalendar());
-            Demonstrate(new JulianCalendar());
+            List<Calendar> calendars = new List<Calendar>
+            {
+                new GregorianCalendar()
+            };
+            AddCalendars(calendars, new JulianCalendar());
+
+            foreach (Calendar calendar in calendars)
+                Demonstrate(calendar);
+
             Console.ReadLine();
         }
+
     }
 }
